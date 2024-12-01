5a8513602e3cd530ca277de86696bee4a26f055c
kks2140501 <kks2140501@naver.com>
2024-12-01 20:37:01 +0900

    SSR로 동작하는 getServerSideProps함수와 next에서 제공하는 타입 공부

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,23 @@ import { ReactNode } from "react";
 import style from "./index.module.css";
 import books from "@/mock/books.json"; //@는 src 폴더를 지칭한다.
 import BookItem from "@/components/book-item";
+import { InferGetServerSidePropsType } from "next";
 
-export default function Home() {
+export const getServerSideProps = () => {
+  // 컴포넌트보다 먼저 실행되어서, 컴포넌트에 필요한 데이터 불러오는 함수
+
+  const data = "hello";
+
+  return {
+    props: {
+      data,
+    },
+  };
+};
+
+export default function Home({
+  data,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <div className={style.container}>
       <section>
